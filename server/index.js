dfd6cd77b603d67afa9f3f5f4a166a07365d3899
MoiseAganze <aganzedev@gmail.com>
2025-01-13 10:48:24 +0100

    ajustements 16 back

--- server/index.js
+++ server/index.js
@@ -18,7 +18,12 @@ app.use(cors(corsOptions));
 app.use("/", authRoutes);
 app.use("/", otherRoutes);
 
-const http = require("http").createServer(app);
+const options = {
+  key: fs.readFileSync("/etc/ssl/private/52.51.182.219.key"),
+  cert: fs.readFileSync("/etc/ssl/certs/52.51.182.219.crt"),
+};
+
+const http = require("https").createServer(options, app);
 require("./socket/io")(http);
 
 http.listen(10000, "0.0.0.0", () => {
